8dbfc812d7293606653818f830a45cb8fa1f9212
Zach Austin <Zach Austin>
2017-05-24 01:34:11 -0400

    Add database functionality to get_posts()

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,12 +1,17 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+DBNAME = "forum"
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  db = psycopg2.connect(DBNAME)
+  c = db.cursor()
+  c.execute("select content, time from posts order by time desc")
+  return c.fetchall()
+  db.close()
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
